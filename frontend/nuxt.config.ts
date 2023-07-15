a135c12f03e2a653480f2ced5d5c753d5811dbbe
Marley <marleyrae1997@gmail.com>
2023-07-14 20:08:37 -0700

    Add vueuse

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,18 +2,23 @@ import {fileURLToPath, URL} from 'node:url'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-typed-router'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    'nuxt-typed-router',
+    '@vueuse/nuxt',
+  ],
   devServer: {
     host: 'bibliothecary.test',
   },
   telemetry: false,
   alias: {
-    '@data': fileURLToPath(new URL('./data', import.meta.url))
+    '@data': fileURLToPath(new URL('./data', import.meta.url)),
   },
   googleFonts: {
     preconnect: true,
     families: {
       Imprima: true,
-    }
-  }
+    },
+  },
 })
